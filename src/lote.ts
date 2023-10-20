50a17519611a6060168da2625533ded65d8b622b
manuelvico0102 <manuelvico0102@correo.ugr.es>
2023-10-20 18:59:05 +0200

    Solucion #10 closes #10
    Se ha creado una clase para modelar el objeto valor Lote

---
+++ src/lote.ts
@@ -0,0 +1,10 @@
+
+export class Lote{
+    private _peso: number;
+    private _volume: number;
+
+    constructor(peso: number, volume: number){
+        this._peso = peso;
+        this._volume = volume;
+    }
+}
