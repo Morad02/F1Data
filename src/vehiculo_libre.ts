18d82523752da0111f775bc36ff4f5c26cd3c89f
manuelvico0102 <manuelvico0102@correo.ugr.es>
2023-10-20 18:54:08 +0200

    Solucion #13 closes #13
    Se ha creado una clase entidad para gestionar el objeto valor vehículo

--- src/vehiculo_libre.ts
+++ src/vehiculo_libre.ts
@@ -5,8 +5,8 @@ export class VehiculoDisponible extends Vehiculo{
     private _pedidos: Pedido[];
     private _disponibilidad: boolean;
 
-    constructor(matricula: string, pedidos: Pedido[], consumo: number, pesoMax: number, volumeMax: number, disponibilidad: boolean){
-        super(matricula, pedidos, consumo, pesoMax, volumeMax);
+    constructor(pedidos: Pedido[], consumo: number, pesoMax: number, volumeMax: number, disponibilidad: boolean){
+        super(pedidos, consumo, pesoMax, volumeMax);
         this._pedidos = pedidos;
         this._disponibilidad = disponibilidad;
     }
