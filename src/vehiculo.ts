66c9c4d98d53213e97a04f11433d3568c6d46763
manuelvico0102 <manuelvico0102@correo.ugr.es>
2023-10-21 18:14:46 +0200

    Solucion #12 closes #12
    Se ha creado una clase para modelar el objeto valor Vehículo

--- src/vehiculo.ts
+++ src/vehiculo.ts
@@ -1,11 +1,9 @@
-import { Pedido } from "./pedido";
-
 export class Vehiculo{
     private _consumo: number;
     private _pesoMax: number;
     private _volumeMax: number;
 
-    constructor(pedidos: Pedido[], consumo: number, pesoMax: number, volumeMax: number){
+    constructor(consumo: number, pesoMax: number, volumeMax: number){
         this._consumo = consumo;
         this._pesoMax = pesoMax;
         this._volumeMax = volumeMax;
