ca9f0786ec8e1d97edd6966e37512928fbdccbbc
manuelvico0102 <manuelvico0102@correo.ugr.es>
2023-10-25 18:08:33 +0200

    Modificado clase pedido y aclaración
    Se ha añadido variables de instacia así como métodos para calcularlos para obtener los datos necesarios para tener la información si un pedido estará en un vehiculo o varios

--- src/pedido.ts
+++ src/pedido.ts
@@ -2,8 +2,28 @@ import { Lote } from "./lote";
 
 export class Pedido{
     private _lotes: Lote[];
+    private _pesoTotal: number;
+    private _volumenTotal: number;
 
     constructor(lotes: Lote[]){
         this._lotes = lotes;
+        this._pesoTotal = this.calcularPesoTotal();
+        this._volumenTotal = this.calcularVolumenTotal();
+    }
+
+    private calcularPesoTotal(): number{
+        let pesoTotal: number = 0;
+        this._lotes.forEach(lote => {
+            pesoTotal += lote.peso;
+        });
+        return pesoTotal;
+    }
+
+    private calcularVolumenTotal(): number{
+        let volumenTotal: number = 0;
+        this._lotes.forEach(lote => {
+            volumenTotal += lote.volumen;
+        });
+        return volumenTotal;
     }
 }
