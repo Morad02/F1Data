bed0a3353366f10b575b780d0f01bc3a63355714
Morad Kharraz Senhaji <kharraz02@correo.ugr.es>
2023-11-02 16:55:01 +0100

    Función para saber si un vehículo asignado puede transportar un lote closes #28
    Comprando peso y volumen de un lote respecto a peso y volumen restante del vehículo

--- src/vehiculo_asignado.ts
+++ src/vehiculo_asignado.ts
@@ -9,4 +9,29 @@ export class VehiculoAsignado{
         this._vehiculo = vehiculo;
         this._lotes = lotes;
     }
+
+    get lotes():Lote[]
+    {
+        return this._lotes
+    }
+
+    get vehiculo():Vehiculo
+    {
+        return this._vehiculo
+    }
+
+    
+
+    puedeCargarLote(lote: Lote): boolean {
+        const pesoCargado = this._lotes.reduce((total, l) => total + l.peso, 0);
+        const volumenCargado = this._lotes.reduce((total, l) => total + l.volume, 0);
+
+        return (
+            pesoCargado + lote.peso <= this._vehiculo.pesoMax &&
+            volumenCargado + lote.volume <= this._vehiculo.volumeMax
+        );
+    }
+
+    
+
 }
