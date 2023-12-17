4ad5c328c772ac55282f5cc4871d05a3bad52590
Morad Kharraz Senhaji <kharraz02@correo.ugr.es>
2023-12-17 09:44:18 +0100

    Funcionalidad para configuración ddel logger closes #65
    En caso de no existir una configuración se usa una por defecto

--- utilities/config_ma.ts
+++ utilities/config_ma.ts
@@ -22,6 +22,24 @@ export class ConfigMa {
         return ConfigMa._instance;
     }
 
-    
+    getLogerConfig(): Map<string, string>
+    {
+        const map = new Map<string, string>();
+        
+        if (ConfigMa.env === undefined || ConfigMa.env.LOGGER === undefined)
+        {
+            map.set("handler", "ArrayHandlers");
+            map.set("level", "DEBUG");
+            map.set("formatter", "[{levelName}] {msg}");
+        }
+        else {
+            const logger = ConfigMa.env.LOGGER;
+                map.set("handler", logger.HANDLER);
+                map.set("level", logger.LEVEL);
+                map.set("formatter", logger.FORMATTER);
+        }
+        
+        return map;
+    }
              
 }
