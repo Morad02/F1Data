2ff06b1c2865315dcc8751bcf99896e85e855e88
Morad Kharraz Senhaji <kharraz02@correo.ugr.es>
2024-01-04 18:25:20 +0100

    Creada getStatus closes #88
    Creado esta función hacemos que responde tenga una única responsabilidad

--- api/api.ts
+++ api/api.ts
@@ -45,13 +45,13 @@ export async function handler(_req: Request)
         if (method === 'POST')
         {
             responseData = await routes[url.pathname]?.[method](id,params);
-            return respond( responseData, "UPDATE" );
+            return respond( responseData, method, params );
 
         } 
         else
         {
             responseData = await routes[url.pathname]?.[method](id);
-            return respond( responseData, method );
+            return respond( responseData, method, params );
         }
                  
         
@@ -59,43 +59,20 @@ export async function handler(_req: Request)
     else if (routes[url.pathname] && params)
     {
         const responseData = await routes[url.pathname]?.[method]('',params);
-        return respond( responseData, "CREATE" );
+        return respond( responseData, method, params );
     }
 
     
-    return respond(null,"ANY");
+    return respond(null,method, params);
 
 
 }
 
-function respond(data:any, method:string)
+function respond(data:any, method:string, params:any)
 {   
-    let status = 200;
+    data = data == null ? {success: false} : {success: true, data};
 
-    if (data == null || data == false)
-    {
-        data = {success: false};
-
-        if (method === 'GET' || method === 'DELETE' || method == 'UPDATE')
-            status = 404;
-        else if (method === 'CREATE' || method === 'ANY')
-            status = 400;
-
-    }
-    else
-    {
-        data = {success: true, data};
-
-        if (method === 'GET' || method === 'UPDATE')
-            status = 200;
-        else if (method === 'DELETE')
-        {
-            return new Response(null,{status: 204});
-
-        }      
-        else if (method === 'CREATE')
-            status = 201;
-    }
+    let status = getStatus(data,method,params);
 
     return new Response(JSON.stringify(data),{
         status,
@@ -105,6 +82,17 @@ function respond(data:any, method:string)
     });
 }
 
+function getStatus(data:any, method:string, params:any):number
+{
+    let status:number;
+
+    if (method === 'GET' || (method == 'POST' && 'id' in params))
+        status = data == null ? 404 : 200 ;
+     else 
+        status = data == null ? 400: 201;
+
+    return status;
+}
 
 async function postLote(id:string,params:any,)
 {   
